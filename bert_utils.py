88df2f29cd8c6410a9381b19505aa921f33364f5
Aleksander Milenkin <60848576+a-milenkin@users.noreply.github.com>
2022-12-19 09:52:46 +0300

    Update bert_utils.py

--- bert_utils.py
+++ bert_utils.py
@@ -1,9 +1,9 @@
 import numpy as np
-from transformers import BertTokenizer, BertForSequenceClassification
+# from transformers import BertTokenizer, BertForSequenceClassification
 
 # Загружаем tokenizer и веса модели
-tokenizer = BertTokenizer.from_pretrained('SkolkovoInstitute/russian_toxicity_classifier')
-model = BertForSequenceClassification.from_pretrained('SkolkovoInstitute/russian_toxicity_classifier')
+# tokenizer = BertTokenizer.from_pretrained('SkolkovoInstitute/russian_toxicity_classifier')
+# model = BertForSequenceClassification.from_pretrained('SkolkovoInstitute/russian_toxicity_classifier')
 
 
 def get_toxic_score(text):
@@ -12,10 +12,10 @@ def get_toxic_score(text):
     Вспомогательная функция, которая по тексту определяет уровень его токсичности
     '''
 
-    batch = tokenizer.encode(text, return_tensors='pt')
-    response = model(batch).logits.detach().numpy()  # Получаем прогноз модели
-    score = np.exp(response[0][1])/sum(np.exp(response[0])) # Оборачиваем в softmax
-
+#     batch = tokenizer.encode(text, return_tensors='pt')
+#     response = model(batch).logits.detach().numpy()  # Получаем прогноз модели
+#     score = np.exp(response[0][1])/sum(np.exp(response[0])) # Оборачиваем в softmax
+    score = 0.5
     score = np.round(score, 3)
 
     if score > 0.6:
